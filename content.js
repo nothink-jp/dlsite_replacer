2df3b67e0023030a32030aeb391d42e7a52a1aa7
crackerjacques <129548912+crackerjacques@users.noreply.github.com>
2024-03-27 21:20:23 +0900

    本文に対応

--- content.js
+++ content.js
@@ -1,11 +1,15 @@
 let isEnabled = true;
 let replacements = [];
 
+function escapeRegExp(string) {
+  return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
 function replaceText(node, wordMap) {
   if (node.nodeType === Node.TEXT_NODE) {
     let content = node.textContent;
     for (let [originalText, replacementText] of wordMap) {
-      const regex = new RegExp(originalText, 'g');
+      const regex = new RegExp(escapeRegExp(originalText), 'gi');
       if (regex.test(content)) {
         const originalContent = content;
         content = content.replace(regex, replacementText);
@@ -16,8 +20,10 @@ function replaceText(node, wordMap) {
     }
     node.textContent = content;
   } else if (node.nodeType === Node.ELEMENT_NODE) {
-    for (let child of node.childNodes) {
-      replaceText(child, wordMap);
+    if (node.nodeName !== 'SCRIPT' && node.nodeName !== 'STYLE') {
+      for (let child of node.childNodes) {
+        replaceText(child, wordMap);
+      }
     }
   }
 }
@@ -31,7 +37,6 @@ function revertReplacements() {
 
 function processDocument() {
   const wordMap = new Map([
-    
     ['ざぁ〜こ♡', 'メスガキ'],
     ['合意なし', 'レイプ'],
     ['閉じ込め', '監禁'],
@@ -57,7 +62,6 @@ function processDocument() {
     ['トランスボイス', '催眠音声'],
     ['つるぺた', 'ロリ'],
     ['畜えち', '獣姦'],
-    
     //テスト用
     //['crackerjaxx', 'モロヘイヤを食う悪魔'],
     //['ダウナー', 'アッパー'],
